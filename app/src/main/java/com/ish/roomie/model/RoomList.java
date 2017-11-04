d16890340e953f90265693463118ad64f08aad78
Isham <ishamkdk190@gmail.com>
2017-11-04 13:46:34 +0000

    Initial skeleton

---
+++ app/src/main/java/com/ish/roomie/model/RoomList.java
@@ -0,0 +1,18 @@
+package com.ish.roomie.model;
+
+/**
+ * Created by 611399999 on 04/11/2017.
+ */
+
+public class RoomList {
+
+    private Rooms rooms;
+
+    public Rooms getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Rooms rooms) {
+        this.rooms = rooms;
+    }
+}
---
+++ app/src/main/java/com/ish/roomie/service/ApiServiceBody.java
@@ -0,0 +1,13 @@
+package com.ish.roomie.service;
+
+/**
+ * Created by 611399999 on 04/11/2017.
+ */
+
+public class ApiServiceBody {
+    final String date;
+
+    public ApiServiceBody(String date) {
+        this.date = date;
+    }
+}
